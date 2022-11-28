4dbf4b63210f9676d63997c8ea455398e5bab716
dhiru0904 <dhirajdevre4@gmail.com>
2022-11-28 15:43:22 +0530

    Added:program is perfect number

---
+++ src/com/logicalprogram/PerfectNumber.java
@@ -0,0 +1,24 @@
+package com.logicalprogram;
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int number, sum = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any number:");
+        number = input.nextInt();
+        if (number > 0) { /* If number is greater than zero */
+            for (int i = 1; i < number; i++) {
+                if (number % i == 0) { /* sum of the factors */
+                    sum = sum + i;
+                }
+            }
+            if (sum == number) {
+                System.out.println(number + " is a perfect number ");
+
+            } else {
+                System.out.println(number + " is not a perfect number ");
+            }
+
+        }
+    }}
